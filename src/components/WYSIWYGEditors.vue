18292a3bd5270d75a1609a54667f1e8d4d69c4fb
Luis <50057252+luisperez1050@users.noreply.github.com>
2022-05-14 23:52:47 -0400

    project started
    new vue project with WYSIWYG

---
+++ src/components/WYSIWYGEditors.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>TinyMCE</p>
+    <editor
+      api-key="no-api-key"
+      :init="{
+        plugins: 'lists link image table code help wordcount'
+      }"
+    />
+    <hr>
+    <p>Quill</p>
+    <QuillEditor theme="snow" />
+  </div>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+export default {
+  name: 'app',
+  components: {
+    'editor': Editor,
+    QuillEditor
+  }
+}
+</script>
