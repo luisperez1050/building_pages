d91018a1e35cab38c04fe0410f0c97e5d9fbb36f
Luis <50057252+luisperez1050@users.noreply.github.com>
2022-05-17 21:13:20 -0400

    added code mirror

--- src/components/WYSIWYGEditors.vue
+++ src/components/WYSIWYGEditors.vue
@@ -23,29 +23,57 @@
       />
       <div class="display-html" v-html="quilleditorHtml"></div>
     </div>
+    <div class="codemirror">
+      <p>Codemirror</p>
+      <codemirror
+        v-model="codemirrorHtml"
+        placeholder="Code goes here..."
+        :style="{ height: '400px' }"
+        :indent-with-tab="true"
+        :tabSize="2"
+        :extensions="extensions"
+        @ready="log('ready', $event)"
+        @change="log('change', $event)"
+        @focus="log('focus', $event)"
+        @blur="log('blur', $event)"
+      />
+      <div class="display-html" v-html="codemirrorHtml"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import Editor from '@tinymce/tinymce-vue'
-import { QuillEditor } from '@vueup/vue-quill'
+import Editor from '@tinymce/tinymce-vue';
+import { Codemirror } from 'vue-codemirror'
+import { javascript } from '@codemirror/lang-javascript'
+import { html } from '@codemirror/lang-html'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { basicSetup } from '@codemirror/basic-setup'
+import { QuillEditor } from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default {
   name: 'app',
   components: {
     'editor': Editor,
-    QuillEditor
+    QuillEditor,
+    Codemirror
+  },
+  setup() {
+    const extensions = [html(), oneDark, basicSetup];
+    return { extensions, log: console.log };
   },
   data() {
     return {
       tinymceHtml: localStorage.getItem('tinyMCEStored'),
       quilleditorHtml: localStorage.getItem('quillStored'),
+      codemirrorHtml: localStorage.getItem('codemirrorStored'),
     };
   },
   updated() {
     localStorage.setItem('tinyMCEStored', this.tinymceHtml ?? '');
     localStorage.setItem('quillStored', this.quilleditorHtml ?? '');
+    localStorage.setItem('codemirrorStored', this.codemirrorHtml ?? '');
   },
 }
 </script>
