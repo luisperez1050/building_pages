131c198a2525effc909004161585d2238ba7e06c
Luis <50057252+luisperez1050@users.noreply.github.com>
2022-06-07 21:26:58 -0400

    added websockts

--- src/components/WebSockets.vue
+++ src/components/WebSockets.vue
@@ -2,6 +2,7 @@
     <div>
         <h2>Connection</h2>
         <p>{{ message }}</p>
+        <button @click="send">Send message</button>
     </div>
 </template>
 
@@ -11,14 +12,24 @@ export default {
     data() {
         return {
             message: '',
+            connection: null,
         };
     },
     mounted() {
-        const connection = new WebSocket('ws://localhost:4000/');
-        connection.onmessage = (event) => {
-            console.log('message', event)
+        this.connection = new WebSocket('ws://localhost:4000/');
+        this.connection.onopen = (event) => {
+            console.log('open', event);
+        }
+        this.connection.onmessage = (event) => {
+            console.log('message', event.data)
             this.message = event.data;
         }
+    },
+    methods: {
+        send() {
+            console.log(this.connection);
+            // this.connection.send('hello from a vue app');
+        },
     },
 }
 </script>
